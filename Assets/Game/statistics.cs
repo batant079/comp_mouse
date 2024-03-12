2057306d29cd5c13eaa2e7f3c0789ad3035271ef
batant079 <anton.asplund06@gmail.com>
2024-03-12 12:57:29 +0100

    f

---
+++ Assets/Game/statistics.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class statistics : MonoBehaviour
+{
+
+    public static statistics instance;
+  
+
+
+
+    public void make_things()
+    {
+
+        // summon this in something to make things i guess
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
+
+
+
